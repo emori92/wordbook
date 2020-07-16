95ecfbeccc2b19f8f4e62ace86ccd9a31ef47370
falcon92 <tktk.tktk.ixjs@gmail.com>
2020-07-16 23:54:38 +0900

    add ranking of number of user followers

--- static/js/notepad/hot.js
+++ static/js/notepad/hot.js
@@ -3,15 +3,18 @@
 
 
 // add or remove class
-const changeDisplay = (id1, id2) => {
+const changeDisplay = (id1, id2, id3) => {
   // get element
   let element1 = document.getElementById(id1);
   let element2 = document.getElementById(id2);
+  let element3 = document.getElementById(id3);
   // change display
   element1.classList.remove('d-none');
   element1.classList.add('d-flex');
   element2.classList.remove('d-flex');
   element2.classList.add('d-none');
+  element3.classList.remove('d-flex');
+  element3.classList.add('d-none');
   // change btn active
   // if (wordbook.classList.contains('active')) {
   //   wordbook.classList.remove('active');
@@ -24,8 +27,10 @@ const changeDisplay = (id1, id2) => {
 
 // btn
 let wordbook = document.getElementById('wordbook-btn');
+let follow = document.getElementById('follow-btn');
 let recommender = document.getElementById('recommender-btn');
 
 // push btn
-wordbook.addEventListener('click', () => { changeDisplay('wordbook', 'recommender') });
-recommender.addEventListener('click', () => { changeDisplay('recommender', 'wordbook') });
+wordbook.addEventListener('click', () => { changeDisplay('wordbook', 'follow', 'recommender') });
+follow.addEventListener('click', () => { changeDisplay('follow', 'wordbook', 'recommender') });
+recommender.addEventListener('click', () => { changeDisplay('recommender', 'wordbook', 'follow') });
